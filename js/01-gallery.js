8b8128de8899797707bedfe4b9ccaba5371ac636
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-04-17 18:24:57 +0300

    add-change-files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'; 
 // Change code below this line
+const list = document.querySelector(".gallery");
+
+
+const itemGal = galleryItems.map(({original, preview, description}) => 
+    `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</li>`).join("");
+
+list.insertAdjacentHTML("afterbegin", itemGal);
+
+
+list.addEventListener("click", onClick);
+
+function onClick (evt){
+    evt.preventDefault();
+    if(!evt.target.classList.contains("gallery__image")){
+        return;
+    }
+    console.dir(evt.target);
+
+    const {alt} = evt.target;
+    const {source} = evt.target.dataset;
+
+    
+    const instance = basicLightbox.create(`
+    <img src="${source}" alt="${alt}">`, {
+        onShow: () => document.addEventListener("keydown",onkeydown),
+        onClose: () => document.removeEventListener("keydown",onkeydown)
+    })
+
+    const onkeydown = (evt)=>{
+        if(evt.code === "Escape"){
+            instance.close();
+        }
+    }
+    instance.show();
+}
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const createMarkup = galleryItems.map(({preview, original, description}) => 
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`).join("");
+
+list.insertAdjacentHTML("afterbegin", createMarkup);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+
+console.table(galleryItems); 
